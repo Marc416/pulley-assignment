a4e3497313ffa0a0462ce3ccd96a22ebb36f575f
이준희 <juni416@naver.com>
2025-01-18 10:17:34 +0900

    feature : 문제지 학생에게 제출하기 테스트코드

---
+++ problem/src/test/kotlin/com/problem/fake/FakePiecePresentToStudentRepository.kt
@@ -0,0 +1,18 @@
+package com.problem.fake
+
+import com.problem.domain.entity.PiecePresentToStudent
+import com.problem.domain.repository.PiecePresentToStudentRepository
+
+class FakePiecePresentToStudentRepository : PiecePresentToStudentRepository {
+    private val map = mutableMapOf<Long, PiecePresentToStudent>()
+    override fun findByPieceIdAndStudentIds(pieceId: Long, studentIds: List<Long>): List<PiecePresentToStudent> {
+        return map.values.filter { it.pieceId == pieceId && it.studentId in studentIds }
+    }
+
+    override fun saveAll(piecePresentToStudent: List<PiecePresentToStudent>): List<PiecePresentToStudent> {
+        piecePresentToStudent.forEach { map[it.id] = it }
+        return piecePresentToStudent
+    }
+
+
+}
