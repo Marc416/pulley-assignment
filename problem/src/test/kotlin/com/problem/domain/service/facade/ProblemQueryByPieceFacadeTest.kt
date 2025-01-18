2d9d3988e168a02ebe0a9b690d6ecdee31210f41
이준희 <juni416@naver.com>
2025-01-18 22:56:37 +0900

    feature : problem_detail 예외테스트 추가

--- problem/src/test/kotlin/com/problem/domain/service/facade/ProblemQueryByPieceFacadeTest.kt
+++ problem/src/test/kotlin/com/problem/domain/service/facade/ProblemQueryByPieceFacadeTest.kt
@@ -1,5 +1,6 @@
 package com.problem.domain.service.facade
 
+import com.problem.application.exception.ApplicationException
 import com.problem.domain.dto.problem.ProblemType
 import com.problem.domain.entity.Piece
 import com.problem.domain.entity.Problem
@@ -20,6 +21,7 @@ import com.problem.fake.FakePieceRepository
 import com.problem.fake.FakeProblemDetailRepository
 import com.problem.fake.FakeProblemRepository
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.BeforeEach
 import kotlin.test.Test
 
@@ -92,5 +94,32 @@ class ProblemQueryByPieceFacadeTest {
         val result = problemQueryByPieceFacade.findProblemDetailByPieceId(studentId, pieceId = piece.id)
         // Assert
         assertThat(result.problems.count()).isEqualTo(piece.problems.count())
+    }
+
+    @Test
+    fun `존재하지 않는 학습지인경우 예외발생`() {
+        // Arrange
+        val teacherId = 1L
+        val studentId = 2L
+        val problem1 = Problem(unitCode = "unit1", type = ProblemType.SELECTION, answer = "1", level = 1)
+        val problem2 = Problem(unitCode = "unit1", type = ProblemType.SELECTION, answer = "2", level = 1)
+        val problem3 = Problem(unitCode = "unit1", type = ProblemType.SELECTION, answer = "3", level = 1)
+        val problem1Saved = problemRepository.save(problem1)
+        val problem2Saved = problemRepository.save(problem2)
+        val problem3Saved = problemRepository.save(problem3)
+        val piece = Piece(
+            title = "piece1",
+            problems = listOf(problem1Saved.id, problem2Saved.id, problem3Saved.id),
+            teacherId = teacherId
+        )
+        pieceRepository.save(piece)
+        pieceCommandService.presentPieceToStudents(
+            piece.id,
+            listOf(studentId),
+            teacherId
+        )
+        // Action, Assert
+        assertThatThrownBy { problemQueryByPieceFacade.findProblemDetailByPieceId(studentId, pieceId = 30L) }
+            .isInstanceOf(ApplicationException::class.java)
     }
 }
