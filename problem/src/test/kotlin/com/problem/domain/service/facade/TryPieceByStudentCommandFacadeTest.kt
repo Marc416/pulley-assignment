ae8bb3b441fd14556ea138438733e5ac1f34570d
이준희 <juni416@naver.com>
2025-01-19 20:22:23 +0900

    feature: 문제지를 채점하는 기능 테스트코드

---
+++ problem/src/test/kotlin/com/problem/domain/service/facade/TryPieceByStudentCommandFacadeTest.kt
@@ -0,0 +1,102 @@
+package com.problem.domain.service.facade
+
+import com.problem.application.controller.request.ProblemAnswer
+import com.problem.domain.dto.problem.ProblemType
+import com.problem.domain.entity.Piece
+import com.problem.domain.entity.PiecePresentToStudent
+import com.problem.domain.entity.Problem
+import com.problem.domain.repository.PiecePresentToStudentRepository
+import com.problem.domain.repository.PieceRepository
+import com.problem.domain.repository.ProblemRepository
+import com.problem.domain.service.PieceQueryService
+import com.problem.domain.service.ProblemQueryService
+import com.problem.domain.usecase.PieceQueryUseCase
+import com.problem.domain.usecase.PieceTryHistoryCommandUseCase
+import com.problem.domain.usecase.ProblemQueryUseCase
+import com.problem.domain.usecase.facade.TryPieceByStudentCommandFacadeUseCase
+import com.problem.fake.FakePiecePresentToStudentRepository
+import com.problem.fake.FakePieceRepository
+import com.problem.fake.FakeProblemRepository
+import io.mockk.every
+import io.mockk.mockk
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import kotlin.test.Test
+
+class TryPieceByStudentCommandFacadeTest {
+
+    private lateinit var pieceRepository: PieceRepository
+    private lateinit var piecePresentToStudentRepository: PiecePresentToStudentRepository
+    private lateinit var problemRepository: ProblemRepository
+
+    private lateinit var tryPieceByStudentCommandFacade: TryPieceByStudentCommandFacadeUseCase
+    private lateinit var pieceQueryUseCase: PieceQueryUseCase
+    private lateinit var problemQueryUseCase: ProblemQueryUseCase
+
+    @BeforeEach
+    fun setUp() {
+        this.pieceRepository = FakePieceRepository()
+        this.problemRepository = FakeProblemRepository()
+        this.piecePresentToStudentRepository = FakePiecePresentToStudentRepository()
+        this.pieceQueryUseCase = PieceQueryService(
+            pieceRepository = pieceRepository,
+            piecePresentToStudentRepository = piecePresentToStudentRepository
+        )
+        this.problemQueryUseCase = ProblemQueryService(
+            problemRepository = problemRepository
+        )
+
+        val pieceTryHistoryCommandUseCase = mockk<PieceTryHistoryCommandUseCase>()
+        every { pieceTryHistoryCommandUseCase.execute(any(), any(), any()) } returns Unit
+
+        tryPieceByStudentCommandFacade = TryPieceByStudentCommandFacade(
+            pieceQueryUseCase = pieceQueryUseCase,
+            problemQueryUseCase = problemQueryUseCase,
+            pieceTryHistoryCommandUseCase = pieceTryHistoryCommandUseCase
+        )
+    }
+
+    @Test
+    fun `학생은 출제받은 문제의 답을 채점받을 수 있다`() {
+        // Arrange
+        val teacherId = 1L
+        val studentId = 2L
+
+        val problem = Problem(
+            unitCode = "unitcode",
+            level = 1,
+            type = ProblemType.SELECTION,
+            answer = "1"
+        )
+        problemRepository.save(problem)
+        val piece = pieceRepository.save(
+            Piece(
+                title = "title",
+                problems = listOf(problem.id),
+                teacherId = teacherId,
+            )
+        )
+        piecePresentToStudentRepository.saveAll(
+            listOf(
+                PiecePresentToStudent(
+                    pieceId = piece.id,
+                    studentId = studentId,
+                    teacherId = teacherId,
+                )
+            )
+        )
+        // Action
+        val result = tryPieceByStudentCommandFacade.tryPiece(
+            studentId = studentId,
+            pieceId = piece.id,
+            studentAnswers = listOf(
+                ProblemAnswer(
+                    problemId = problem.id,
+                    answer = "1"
+                ),
+            )
+        )
+        // Assert
+        assertThat(result.first().isCorrect).isEqualTo(true)
+    }
+}
