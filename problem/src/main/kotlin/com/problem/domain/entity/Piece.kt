1f5b17d1474d1c3cab1ec56a537bd237186f628a
이준희 <juni416@naver.com>
2025-01-21 08:59:34 +0900

    feature: 문제 50개이상 넘지 않도록

--- problem/src/main/kotlin/com/problem/domain/entity/Piece.kt
+++ problem/src/main/kotlin/com/problem/domain/entity/Piece.kt
@@ -1,6 +1,8 @@
 package com.problem.domain.entity
 
+import com.problem.application.common.httpresponse.CodeEnum
 import com.problem.application.config.converter.LongListConverter
+import com.problem.application.exception.ApplicationException
 import jakarta.persistence.*
 import java.time.LocalDateTime
 
@@ -13,6 +15,12 @@ class Piece(
     val teacherId: Long,
     val createdAt: LocalDateTime = LocalDateTime.now()
 ) {
+    init {
+        if (problems.size > 50) {
+            throw ApplicationException(CodeEnum.FRS_003, "The number of problems cannot exceed 50.")
+        }
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0L
